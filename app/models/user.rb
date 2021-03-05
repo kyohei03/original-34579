7ee491b05e8de10e48b3b2f2eac5abc05a3e2217
kyohei03 <kyohei03x@gmail.com>
2021-03-05 18:37:10 +0900

    マイグレーション設定・アソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :memos
+  
   with_options presence: true do
     validates :nickname
     validates :department
