d936c22a21ee6f058b8c3c7e8be176cbea8cec54
kyohei03 <kyohei03x@gmail.com>
2021-03-06 18:49:01 +0900

    commit

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,8 +1,19 @@
 class MemosController < ApplicationController
 
   def index
+    @memos = Memo.all
   end
 
   def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memos.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'memos/index'
   root to: "memos#index"
+  resources :memos do
+    end
 end
