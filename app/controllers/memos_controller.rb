db52075c04aee488a1b210ceb6bb1427763c9db4
kyohei03 <kyohei03x@gmail.com>
2021-03-15 20:14:09 +0900

    commit

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -20,20 +20,17 @@ class MemosController < ApplicationController
   end
 
   def show
-    @memo = Memo.find(params[:id])
     @comment = Comment.new
     @comments = @memo.comments
   end
 
   def edit
-    @memo = Memo.find(params[:id])
     unless current_user == @memo.user
       redirect_to action: :index
     end
   end
 
   def update
-    @memo = Memo.find(params[:id]) 
     if @memo.update(memo_params)
       redirect_to memo_path
     else
@@ -42,7 +39,6 @@ class MemosController < ApplicationController
   end
 
   def destroy
-    @memo = Memo.find(params[:id]) 
     @memo.destroy
     redirect_to root_path
   end
